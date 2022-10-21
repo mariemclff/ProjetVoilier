6d9002444709d9fbb427ee7bbf2f0a6d797b2f43
Chaouch Sirine <sirine.chaouch2001@gmail.com>
2022-10-21 18:16:56 +0200

    pwm explication

--- MesDrivers/Include/macro.h
+++ MesDrivers/Include/macro.h
@@ -2,9 +2,14 @@
 #define MACRO_H
 #include "stm32f10x.h"
 
-#define Bidon //GPIOA->Pin machin truc// a comple te r
+#define Bidon //GPIOC8->entrée cap
+#define Bidon2 //GPIOC9->sortie cap
+#define Bidon3 //GPIOC10->sortie PWM
+
+
 #define Bidouille 0x08  // a comple te r
 
+#define Tim_Cap TIM2 
 #define Tim_Girouette TIM3 
 
 #endif
---
+++ MesDrivers/Source/PWM.c
@@ -0,0 +1,4 @@
+
+ARR/CRX : rapport entre front montant et front descendant 
+72MHz et 20 khz de la pwm a mettre en forme pour avoir la bonne relation 
+PSC prescaler permet de ralentir la freq pour trouver un diviseur commun
