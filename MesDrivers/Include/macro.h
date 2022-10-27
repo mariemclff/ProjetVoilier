eee55894af954e2c38676562f391ef2e5341815a
Marie Mecaliff <mecaliff@insa-toulouse.fr>
2022-10-27 11:08:49 +0200

    New ignore

--- MesDrivers/Include/macro.h
+++ MesDrivers/Include/macro.h
@@ -2,14 +2,14 @@
 #define MACRO_H
 #include "stm32f10x.h"
 
-#define Bidon //GPIOC8->entrée cap
+#define TimerBordage TIM3
+#define GPIOBordage GPIOA 
+#define PinBordage 8 //PA8
+
+#define Bidon1  //GPIOC8 -> entree cap
 #define Bidon2 //GPIOC9->sortie cap
 #define Bidon3 //GPIOC10->sortie PWM
 
-
-#define Bidouille 0x08  // a comple te r
-
-#define Tim_Cap TIM2 
-#define Tim_Girouette TIM3 
+#define Tim_Cap TIM2  
 
 #endif
